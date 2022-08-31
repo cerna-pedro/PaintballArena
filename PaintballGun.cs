9c80bdd768c1924479ff4dd8d2d18fc6bca83078
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-08-30 19:29:17 -0500

    Add project files.

---
+++ PaintballGun.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintballArena
+{
+    internal class PaintballGun
+    {
+        public const int MAGAZINE_SIZE = 16;
+
+        private int balls = 0;
+        public int BallsLoaded { get; private set; }
+        public int Balls
+        {
+            get
+            {
+                return balls;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    balls = value;
+                }
+                Reload();
+            }
+        }
+
+
+
+
+        public bool IsEmpty()
+        {
+            return (BallsLoaded == 0);
+        }
+
+        public void Reload()
+        {
+            if (balls > MAGAZINE_SIZE)
+            {
+                BallsLoaded = MAGAZINE_SIZE;
+            }
+            else
+            {
+                BallsLoaded = balls;
+            }
+        }
+        public bool Shoot()
+        {
+            if (BallsLoaded == 0)
+            {
+                return false;
+            }
+            BallsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
